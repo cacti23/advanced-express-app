b4646f2bd806759be5eabfb28c10a50485d65114
cacti23 <patel.aditya2014@gmail.com>
2023-03-20 12:42:49 +0530

    feat: redis implemented

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose')
-const authenticate = require('@middleware/authenticate')
+const mongoose = require("mongoose");
+const authenticate = require("@middleware/authenticate");
 
-const Blog = mongoose.model('Blog')
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', authenticate, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", authenticate, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
-    })
-    res.send(blog)
-  })
+      _id: req.params.id,
+    });
+    res.send(blog);
+  });
 
-  app.get('/api/blogs', authenticate, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id })
-    res.send(blogs)
-  })
+  app.get("/api/blogs", authenticate, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id });
+    res.send(blogs);
+  });
 
-  app.post('/api/blogs', authenticate, async (req, res) => {
-    const { title, content } = req.body
+  app.post("/api/blogs", authenticate, async (req, res) => {
+    const { title, content } = req.body;
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
-    })
+      _user: req.user.id,
+    });
     try {
-      await blog.save()
-      res.send(blog)
+      await blog.save();
+      res.send(blog);
     } catch (err) {
-      res.send(400, err)
+      res.send(400, err);
     }
-  })
-}
+  });
+};
---
+++ services/cache.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const url = "redis://127.0.0.1:6379";
+const client = redis.createClient(url);
+const util = require("util");
+
+// client.get takes callback but we use promisify to change
+
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+  // here this represents to the current running query
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  // see if you have a value for 'key' in redis
+  const cachedValue = await client.get(key);
+
+  // if we do return that
+  if (cachedValue) {
+    // this.model represents the class of model on which this query is running
+    const doc = JSON.parse(cachedValue);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
+};

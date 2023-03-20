efe82f3b0955e5754715f74629b42064a74d1e15
cacti23 <patel.aditya2014@gmail.com>
2023-03-20 13:13:36 +0530

    chore: add cache chainable function to mongoose Query

--- services/cache.js
+++ services/cache.js
@@ -10,8 +10,17 @@ client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+  // to make cache chainable
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
   // here this represents to the current running query
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {

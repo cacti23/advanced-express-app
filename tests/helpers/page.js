b2ce1c71607e8f98411b305e3bbe1e8cf6a27b4c
cacti23 <patel.aditya2014@gmail.com>
2023-04-09 14:08:18 +0530

    fix(travis): update .travis.yml

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,8 +5,8 @@ const userFactory = require("../factories/userFactory");
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox"],
+      headless: false,
+      // args: ["--no-sandbox"],
     });
 
     const page = await browser.newPage();
